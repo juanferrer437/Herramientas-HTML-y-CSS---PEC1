46522ead70849796fd8d7a0bd72ac8081a364ac2
juanferrer437 <juanferrer437@uoc.edu>
2022-04-12 16:12:43 +0200

    a

---
+++ js/jquery.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+    $('#pgwSlider').pgwSlider();
+    alert ("Hola");
+});
+
+$(function () {
+    $(".toggle").on("click", function () {
+        if ($(".item").hasClass("active")) {
+            $(".item").removeClass("active");
+            $(this).find("a").html("<i class='fas fa-bars'></i>");
+        } else {
+            $(".item").addClass("active");
+            $(this).find("a").html("<i class='fas fa-times'></i>");
+        }
+    });
+
+
+
+    
+});
